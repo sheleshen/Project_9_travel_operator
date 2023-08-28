505e28c79246c3daf0ab8eaae3e0d52989e98f5e
sheleshen <sheleshnewaanna@gmail.com>
2023-08-28 20:35:59 +0300

    mob.ver

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,106 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    
+    // colors: {
+      // transparent: 'transparent',
+      // current: 'currentColor',
+      // // моя палитра цветов
+      // // MAIN
+      // 'main-sand': '#b68155',
+      // 'main-sand-secondary': '#b47754',
+      // 'main-sand-dark': '#65554c',
+
+      // 'main-grey-blue': '#c7c8cb',
+      // 'main-grey': '#6d7478',
+      // 'main-grey-dark-blue': '#63626d',
+      // 'main-brown': '#2b2523',
+
+      // // Secondary MAIN
+      // 'main-sand2': '#b67e55',
+      // 'main-sand3': '#b57c55',
+      // 'main-sand4': '#b57954',
+
+      // 'main-grey-sand': '#c3b7b3',
+      // 'main-grey-sand2': '#bfa69b',
+      // 'main-grey-sand3': '#bc9684',
+      // 'main-grey-sand4': '#b8856c',
+
+      // 'main-contrast-blue': '#497eaa',
+      // 'main-contrast-blue2': '#4b8bab',
+      // 'main-contrast-grey-blue': '#9aaab3',
+      // 'main-contrast-grey-blue2': '#727d81',
+      // 'main-contrast-grey': '#d4dadc',
+
+      // 'main-contrast-brown': '#383734',
+      // 'main-contrast-brown2': '#5b5652',
+      // 'main-contrast-brown3': '#6d645b',
+      // 'main-contrast-brown4': '#928b87',
+      // 'main-contrast-brown5': '#9c9d92',
+
+      // // BG
+      // 'bg-white': '#ffffff',
+      // 'bg-light': '#f8f4f0',
+      // 'bg-light2': '#f2e8e0',
+      // 'bg-light3': '#ebddd1',
+      // 'bg-light4': '#e4d1c1',
+      // 'bg-light5': '#dec6b2',
+
+      // // STROKE
+      // 'str-grey': '#f2f2f2',
+      // 'str-dark-grey': '#dbe5f0',
+      // // 'str-dark-grey': '#d5d6d8',
+
+      // ТЕКСТ
+      // 'text-black': '#000000',
+      // 'text-brown-dark4': '#110c08',
+      // 'text-brown-dark3': '#21170f',
+      // 'text-brown-dark2': '#322317',
+      // 'text-brown-dark': '#422f1f',
+      // 'text-brown': '#63462e',
+      // 'text-brown-light': '#63462e',
+      // 'text-brown-light2': '#745236',
+      // 'text-brown-light3': '#845e3e',
+      // 'text-brown-light4': '#956a46',
+      // 'text-brown-light5': '#a5754d',
+      // 'text-brown-light6': '#b68155',
+      // 'text-white': '#ffffff',
+
+      // 'text-grey-light': '#a8a8a8',
+      // 'text-grey': '#6d7478',
+
+      // // BTN
+      // // Primary
+      // 'btn-pr-bg': '#b68155',
+      // 'btn-pr-t': '#ffffff',
+
+      // 'btn-pr-bgh': '#b47454',
+      // 'btn-pr-th': '#ffffff',
+
+      // 'btn-pr-bgcl': '#b3663f',
+      // 'btn-pr-tcl': '#ffffff',
+
+      // // Secondary
+      // 'btn-sec-bg': '#928b87',
+      // 'btn-sec-t': '#ffffff',
+
+      // 'btn-sec-bgh': '#716b68',
+      // 'btn-sec-th': '#ffffff',
+
+      // 'btn-sec-bgcl': '#928b87',
+      // 'btn-sec-tcl': '#ffffff',
+
+      // 'btn-sec-bgdes': '#928b8700',
+      // 'btn-sec-tdes': '#928b87',
+
+      // // Form
+      // 'form-bg': '#ffffff',
+      // 'form-str': '#d5e5ee',
+      // 'form-text': '#919191',
+      // 'form-text-lead-in': '#363636',
+      // 'form-error': '#d84e4e',
+    // }
+    
   },
   plugins: [],
 }
-
